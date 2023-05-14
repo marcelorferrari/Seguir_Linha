2a39ed94f32cdfc67334be033b6731efe2bb356d
marcelorferrari <marcelorferrari@yahoo.com.br>
2023-05-14 15:54:24 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,5 +1,25 @@
-input.onButtonPressed(Button.A, function () {
-    while (true) {
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (ligado == 0) {
+        ligado = 1
+    } else {
+        ligado = 1
+    }
+})
+let ligado = 0
+let LCal = pins.analogReadPin(AnalogPin.P0)
+let RCal = pins.analogReadPin(AnalogPin.P2)
+ligado = 0
+robotbit.MotorRun(robotbit.Motors.M1A, 0)
+robotbit.MotorRun(robotbit.Motors.M2B, 0)
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    if (ligado == 1) {
         if (pins.analogReadPin(AnalogPin.P0) <= LCal - 30) {
             basic.showLeds(`
                 . . # . .
@@ -8,8 +28,8 @@ input.onButtonPressed(Button.A, function () {
                 . . # # .
                 . . # . .
                 `)
-            motobit.setMotorSpeed(Motor.Left, MotorDirection.Reverse, 75)
-            motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 25)
+            robotbit.MotorRun(robotbit.Motors.M1A, 75)
+            robotbit.MotorRun(robotbit.Motors.M2B, -25)
             basic.pause(50)
         } else if (pins.analogReadPin(AnalogPin.P2) <= RCal - 30) {
             basic.showLeds(`
@@ -19,8 +39,8 @@ input.onButtonPressed(Button.A, function () {
                 . # # . .
                 . . # . .
                 `)
-            motobit.setMotorSpeed(Motor.Right, MotorDirection.Reverse, 75)
-            motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 25)
+            robotbit.MotorRun(robotbit.Motors.M1A, -25)
+            robotbit.MotorRun(robotbit.Motors.M2B, 75)
             basic.pause(50)
         } else {
             basic.showLeds(`
@@ -30,14 +50,8 @@ input.onButtonPressed(Button.A, function () {
                 . . # . .
                 . . # . .
                 `)
-            motobit.setMotorSpeed(Motor.Left, MotorDirection.Reverse, 40)
-            motobit.setMotorSpeed(Motor.Right, MotorDirection.Reverse, 40)
+            robotbit.MotorRun(robotbit.Motors.M1A, 50)
+            robotbit.MotorRun(robotbit.Motors.M2B, 50)
         }
     }
 })
-let RCal = 0
-let LCal = 0
-LCal = pins.analogReadPin(AnalogPin.P0)
-RCal = pins.analogReadPin(AnalogPin.P2)
-robotbit.MotorRun(robotbit.Motors.M1A, 100)
-robotbit.MotorRun(robotbit.Motors.M1B, 100)
